67b3c62cfea0d001a4a5becbbec77e64fdec73f6
Sergey <104456968+Solod-S@users.noreply.github.com>
2026-06-14 17:42:28 +0300

    Refactor project structure: update settings for templates and static files, enhance URL routing, and implement new course templates with improved styling.

--- base/urls.py
+++ base/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    path('', include("shop.urls")),
+    path('', RedirectView.as_view(url='/shop/')),
     path('admin/', admin.site.urls),
     path('shop/', include("shop.urls")),
 ]
