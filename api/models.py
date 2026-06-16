6df2e7469861160d5957e57347bf89e36a92f7fc
Sergey <104456968+Solod-S@users.noreply.github.com>
2026-06-16 17:55:13 +0300

    Add django-tastypie to Pipfile and update related documentation; enhance API resource methods for category and course handling

--- api/models.py
+++ api/models.py
@@ -7,8 +7,9 @@ from .authentication import CustomAuthentication
 
 
 class CategoryResource(ModelResource):
-    # /api/v1/categories
-    # /api/v1/categories/?limit=1&offset=1
+    # /api/v1/categories                    GET
+    # /api/v1/categories/?limit=1&offset=1  GET
+
     class Meta:
         queryset = Category.objects.all()
         resource_name = "categories"
@@ -16,8 +17,9 @@ class CategoryResource(ModelResource):
 
 
 class CourseResource(ModelResource):
-    # /api/v1/courses
-    # /api/v1/courses/?limit=1&offset=1
+    # /api/v1/courses                    GET, POST
+    # /api/v1/courses/?limit=1&offset=1  GET,
+    # /api/v1/courses/1/                 GET, DELETE
 
     class Meta:
         queryset = Course.objects.all()
@@ -25,3 +27,24 @@ class CourseResource(ModelResource):
         allowed_methods = ["get", "post", "delete"]
         authentication = CustomAuthentication()
         authorization = Authorization()
+
+# hydrate - данные идут к серверу
+# dehydrate - данные идут к клиенту
+
+# для нормального приема category_id из body
+    def hydrate(self, bundle):
+        bundle.obj.category_id = bundle.data["category_id"]
+        return bundle
+
+# добавляем category_id в тело ответа
+    def dehydrate(self, bundle):
+        bundle.data["category_id"] = bundle.obj.category_id
+        return bundle
+
+# добавляем все заголовки большими в тело ответа
+    def dehydrate_title(self, bundle):
+        return bundle.data["title"].upper()
+
+# делаем все reviews_qty строками в тело ответа
+    def dehydrate_reviews_qty(self, bundle):
+        return str(bundle.data["reviews_qty"])
